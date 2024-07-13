7e9f33f32e7badd5889146aa32700faf0b022ddd
Kacper <126982983+KacperZimmer@users.noreply.github.com>
2024-07-13 20:46:22 +0200

    work in progress

---
+++ include/ObserverSubject.h
@@ -0,0 +1,16 @@
+//
+// Created by Kacper Zimmer on 13/07/2024.
+//
+
+#ifndef EXAMPLE_OBSERVERSUBJECT_H
+#define EXAMPLE_OBSERVERSUBJECT_H
+
+
+
+class ObserverSubject {
+
+};
+
+
+
+#endif //EXAMPLE_OBSERVERSUBJECT_H
